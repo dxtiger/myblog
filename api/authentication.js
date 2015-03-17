a2ecbc8d5860ff90e02a808a1693e80e66c7a492
luyawei <luyawei@yoka.com>
2015-03-17 11:14:58 +0800

    auth

--- api/authentication.js
+++ api/authentication.js
@@ -1,11 +1,11 @@
-var admin_id = '550292cfa0bed4a80b2a9b64', // 管理员id,目前仅允许管理员发布，编辑文章,嗯，我的博客我做主
+var admin = 'ryw401@163.com', // 管理员id,目前仅允许管理员发布，编辑文章,嗯，我的博客我做主
 	model = require('../model/index'),
 	user = model.User;
 
 function author(req,res,next) {
 	var token = req.cookies.token;
 	if(req.session && req.session.uid){
-		if(admin_id != req.session.uid._id){
+		if(admin != req.session.uid.email){
 			res.redirect('/');
 			return;
 		}
